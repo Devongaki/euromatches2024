0d013a046f5ee7ebc29d6d6a2e0c4aba6aa171b9
willyongaks <134366767+Devongaki@users.noreply.github.com>
2024-06-27 18:07:02 +0200

    implemented match scheduled and match finished with colors

--- src/component/DateGroup/DateGroup.js
+++ src/component/DateGroup/DateGroup.js
@@ -1,22 +1,18 @@
-// src/components/DateGroup.js
 import React from "react";
 import "./DateGroup.css";
 import { format } from "date-fns";
 import { getMatchStatus } from "../../utils/matchStatusMapper";
 
-const DateGroup = ({ date, matches, groupName = [] }) => {
-  const formattedDate = format(new Date(date), "EEEE dd MMMM yyy");
+const DateGroup = ({ date, matches = [] }) => {
+  const formattedDate = format(new Date(date), "EEEE dd MMMM yyyy");
+
   return (
     <div className="date-group">
       <h2 className="date-group-heading">{formattedDate}</h2>
       <div className="date-group-item">
         {matches.map((match, index) => (
           <div key={index} className="match">
-            <div>
-              <p>{match.groupName}</p>
-              <p>{match.stadium.name}</p>
-            </div>
-
+            <p className="group-name">{match.groupName}</p>
             <p className="match-results">
               {match.homeTeam.name}{" "}
               <span className="match-results-score">
@@ -29,9 +25,24 @@ const DateGroup = ({ date, matches, groupName = [] }) => {
                 {match.result.awayScore90}
               </span>
             </p>
-            <p className="match-status-ongoing">
-              Status: {getMatchStatus(match.matchStatusId)}
-            </p>
+            {match.matchStatusId === 1 ? (
+              <p className="match-status-scheduled">
+                Status:{" "}
+                <span>
+                  Scheduled{" "}
+                  {format(
+                    new Date(match.timestamp),
+                    "EEEE dd MMMM yyyy, HH:mm"
+                  )}
+                </span>
+              </p>
+            ) : (
+              <p className="match-status-done">
+                Status: <span>{getMatchStatus(match.matchStatusId)}</span>
+              </p>
+            )}
+
+            <p className="match-stadium">Stadium: {match.stadium.name}</p>
           </div>
         ))}
       </div>
